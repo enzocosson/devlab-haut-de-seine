c85d0ec615715bdf9728bb7ca947492583dccf7b
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2025-03-08 10:59:26 +0100

    update dynamic collection points in db

--- back-end/models/collectionpoint.js
+++ back-end/models/collectionpoint.js
@@ -1,30 +1,38 @@
 module.exports = (sequelize, DataTypes) => {
-    const CollectionPoint = sequelize.define(
-        'CollectionPoint', // Nom logique du modèle
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-            },
-            nom: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            adresse: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            type: {
-                type: DataTypes.STRING,
-                allowNull: true,
-            },
-        },
-        {
-            tableName: 'collectionpoint', // Correspond exactement au nom de la table dans PostgreSQL
-            timestamps: false, // Désactive les colonnes `createdAt` et `updatedAt`
-        }
-    );
+	const CollectionPoint = sequelize.define(
+		"CollectionPoint", // Nom logique du modèle
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			nom: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			adresse: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			type: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			latitude: {
+				type: DataTypes.REAL,
+				allowNull: true,
+			},
+			longitude: {
+				type: DataTypes.REAL,
+				allowNull: true,
+			},
+		},
+		{
+			tableName: "collectionpoint", // Correspond exactement au nom de la table dans PostgreSQL
+			timestamps: false, // Désactive les colonnes `createdAt` et `updatedAt`
+		}
+	);
 
-    return CollectionPoint;
+	return CollectionPoint;
 };
--- front-end/src/pages/Dashboard/Dashboard.jsx
+++ front-end/src/pages/Dashboard/Dashboard.jsx
@@ -20,25 +20,10 @@ import { jwtDecode } from "jwt-decode";
 
 function Dashboard() {
 	const mapRef = useRef();
+	const [selectedPoint, setSelectedPoint] = useState(null);
 	const navigate = useNavigate(); // Hook pour la navigation
 	const [logged, setLogged] = useState("");
-	const [pointsRelais, setPointsRelais] = useState([
-		{
-			id: 1,
-			name: "Mairie de Boulogne-Billancourt",
-			lat: 48.8397,
-			lng: 2.2399,
-		},
-		{ id: 2, name: "Mairie de Nanterre", lat: 48.8925, lng: 2.2067 },
-		{ id: 3, name: "Mairie de Courbevoie", lat: 48.8977, lng: 2.2567 },
-		{ id: 4, name: "Mairie de Colombes", lat: 48.9226, lng: 2.254 },
-		{ id: 5, name: "Mairie de Rueil-Malmaison", lat: 48.8779, lng: 2.1799 },
-		{ id: 6, name: "Mairie de Clamart", lat: 48.7988, lng: 2.2667 },
-		{ id: 7, name: "Mairie d’Issy-les-Moulineaux", lat: 48.822, lng: 2.2772 },
-		{ id: 8, name: "Mairie de Suresnes", lat: 48.8714, lng: 2.2295 },
-		{ id: 9, name: "Mairie de Levallois-Perret", lat: 48.893, lng: 2.288 },
-		{ id: 10, name: "Mairie de Meudon", lat: 48.8133, lng: 2.2384 },
-	]);
+	const [collectionPoints, setCollectionPoints] = useState([]);
 
 	useEffect(() => {
 		setLogged(false);
@@ -54,23 +39,6 @@ function Dashboard() {
 			}
 		else navigate("/login");
 	}, []);
-
-	const [selectedPoint, setSelectedPoint] = useState(null);
-
-	const handlePointClick = (point) => {
-		setSelectedPoint(point);
-		const map = mapRef.current;
-		if (map) {
-			map.flyTo([point.lat, point.lng], 15, { duration: 1.5 });
-		}
-	};
-
-	const handleDepositClick = () => {
-		if (selectedPoint) {
-			// Redirige vers la page de formulaire avec le nom de la mairie sélectionnée
-			navigate(`/formulaire?mairie=${encodeURIComponent(selectedPoint.name)}`);
-		}
-	};
 
 	const handleLogout = () => {
 		localStorage.removeItem("token");
