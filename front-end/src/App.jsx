007f45461bfd01a1dd6e059f86703b8b89aad2c6
Céline Eap <celine-chhun-huy.eap@edu.devinci.fr>
2025-03-12 01:17:52 +0100

    feat: add landing page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,36 +1,29 @@
-import { useEffect } from "react";
 import {
-  Route,
   BrowserRouter as Router,
-  Routes,
   useLocation,
-  Link,
 } from "react-router-dom";
 import styles from "./App.module.scss";
 import { AppContextProvider } from "./Context";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import AnimatedRoutes from "./AnimatedRoutes";
-import LoginPage from "./pages/LoginPage/LoginPage.jsx";
-import RegisterPage from "@/pages/LoginPage/RegisterPage.jsx";
 
 function App() {
+    const location = useLocation();
+    const showHeader =
+        location.pathname.startsWith("/login") ||
+        location.pathname.startsWith("/register");
+    const showFooter = location.pathname === "/"
 
-
-  const location = useLocation();
-  const showHeader =
-    location.pathname.startsWith("/login") ||
-    location.pathname.startsWith("/register");
-
-  return (
-    <div className={styles.main}>
-      {/* {!isDashboard && <Header />} */}
-      {!showHeader && <Header />}
-      <div className={styles.container}>
-        <AnimatedRoutes />
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.main}>
+            {!showHeader && <Header />}
+            <div className={styles.container}>
+                <AnimatedRoutes />
+            </div>
+            {showFooter && <Footer />}
+        </div>
+    );
 }
 
 export default function AppWrapper() {
