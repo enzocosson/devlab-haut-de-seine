31ccaec9e1ec11e77dfa5584c82eb6a708c602f9
enzocosson <enzolemercier@gmail.com>
2025-03-13 19:36:56 +0100

    fix : routage

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,14 +1,12 @@
-/* eslint-disable no-undef */
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src/"),
     },
   },
 });
