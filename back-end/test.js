36d705ba3fc67bba92133df058f240e3d6b68e82
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2025-03-14 09:52:09 +0100

    update install pg

--- back-end/test.js
+++ back-end/test.js
@@ -3,7 +3,7 @@ const { User } = require('./models');
 const hashAndUpdatePassword = async () => {
     const email = 'admin@test.com'; // Remplace par l'email de ton admin
     await User.update(
-        { password: hashedPassword },
+        { password: "adminpassword" },
         { where: { email } }
     );
     console.log('Mot de passe mis à jour avec succès !');
